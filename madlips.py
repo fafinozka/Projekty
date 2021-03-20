63f667c6669360957e72f72340dcd40476c6ceb9
František Jaburek <fafinozka@gmail.com>
2021-03-20 23:17:50 +0100

    nedokoncena prace madlips

---
+++ madlips.py
@@ -0,0 +1,35 @@
+#na zacatku se zepta na pet podstatnych jmen, pet pridavnych, pet sloves. Nasledne se nahoodne slozi pet vet,ktere musi byt kazda jako jeden string. Pak se to spoji a bude z toho pribeh jako jeden string
+#POTOOM SE PROVEDE SPOCITANI KOLIK JE JAKYCH PISMENEK V CELEM TEXTU.
+import random
+
+noun_number = 0
+noun_list = []
+adjective_number = 0
+adjective_list = []
+verb_number = 0
+verb_list = []
+
+while noun_number < 5:
+	noun = input("please enter one noun: ")
+	noun_list.append(noun)
+	noun_number += 1
+
+while adjective_number < 5:
+	adjective = input("please enter one adjective: ")
+	adjective_list.append(adjective)
+	adjective_number += 1
+
+while verb_number < 5:
+	verb = input("please enter one verb: ")
+	verb_list.append(verb)
+	verb_number += 1
+
+random.shuffle(noun_list)
+random.shuffle(adjective_list)
+random.shuffle(verb_list)
+
+print(adjective_list[0], noun_list[0], verb_list[0])
+print(adjective_list[1], noun_list[1], verb_list[1])
+print(adjective_list[2], noun_list[2], verb_list[2])
+print(adjective_list[3], noun_list[3], verb_list[3])
+print(adjective_list[4], noun_list[4], verb_list[4])
