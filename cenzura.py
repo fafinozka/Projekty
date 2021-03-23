93892a3c3003550bd5989b0a644b269828394845
František Jaburek <fafinozka@gmail.com>
2021-03-23 22:14:39 +0100

    zaciname cenzuru

--- cenzura.py
+++ cenzura.py
@@ -1,3 +1,8 @@
 vstup = open("vstup.txt", "rt")
 a=vstup.read()
-print(a)
+
+cenzura = open("cenzura.txt", "r")
+content_cenzura = cenzura.read()
+cenzura_list = content_cenzura.split(",")
+cenzura.close()
+print(a.replace(cenzura_list[0], len(cenzura_list[0]) * "*"))
