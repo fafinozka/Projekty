8c44085bf8b6f5386486a9720369b466183974c1
František Jaburek <fafinozka@gmail.com>
2021-03-25 21:34:48 +0100

    cenzura skoro dokoncena a zasek :`(

--- cenzura.py
+++ cenzura.py
@@ -1,11 +1,15 @@
-vstup = open("vstup.txt", "rt")
-a=vstup.read()
+import re
+vstup = open ("vstup.txt", "rt")
+a = vstup.read()
+a2 = a.lower()
 
 cenzura = open("cenzura.txt", "r")
 content_cenzura = cenzura.read()
-cenzura_list = content_cenzura.split("\n")
+cenzura_list = content_cenzura.lower().split("\n")
 cenzura.close()
 
 for element in cenzura_list:
-	a=(a.replace(element, int(len(element)) * "*"))
-print(a)
+	a2 = (a2.replace(element, int(len(element)) * "*"))
+print(a2)
+
+
