1fabb4c08552b66da39ae48bc9ab65fe76b32057
František Jaburek <fafinozka@gmail.com>
2021-03-25 22:45:01 +0100

    cenzura hotovo

--- cenzura.py
+++ cenzura.py
@@ -1,15 +1,36 @@
 import re
 vstup = open ("vstup.txt", "rt")
-a = vstup.read()
-a2 = a.lower()
+text = vstup.read()
+text_malej = text.lower()
 
 cenzura = open("cenzura.txt", "r")
 content_cenzura = cenzura.read()
 cenzura_list = content_cenzura.lower().split("\n")
 cenzura.close()
 
+text_cenzura = text_malej
+
 for element in cenzura_list:
-	a2 = (a2.replace(element, int(len(element)) * "*"))
-print(a2)
+	text_cenzura = (text_cenzura.replace(element, int(len(element)) * "*"))
+print(text_cenzura)
 
 
+vysledek={}
+for element in cenzura_list:
+	text_krajeni = text_malej
+	pozice_abs=0
+	pole=[]
+	while True:
+		pozice = text_krajeni.find(element)
+		if pozice == -1:
+			vysledek[element]=pole
+			break
+		else:
+			#print(f"Na pozici: {pozice_abs} je slovo: {element} ")
+			text_krajeni = text_krajeni[(pozice+len(element)):]
+
+			pozice_abs += pozice
+			pole.append(pozice_abs)
+
+for i in vysledek:
+	print(i, vysledek[i])
