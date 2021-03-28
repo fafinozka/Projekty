fd771dbad97c4c22fdbfb0d91455ebeb0a134868
František Jaburek <fafinozka@gmail.com>
2021-03-28 15:27:41 +0200

    Zacatek grafy...

--- pocitac_pismen.py
+++ pocitac_pismen.py
@@ -1,10 +1,11 @@
+from time import sleep
 
-"----------------------------------------------------------"
+import matplotlib.pyplot as plt
+#"----------------------------------------------------------"
 
 pismenka = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-
-"----------------------------------------------------------"
+#"----------------------------------------------------------"
 
 ABC_count = open("ABC_count.txt", "rt")
 text = ABC_count.read()
@@ -26,6 +27,15 @@ for i in (sorted(vysledek)):
 total = 0
 for i in vysledek:
 	total += vysledek[i]
-
+dict = {}
 for i in (sorted(vysledek)):
-	print(f"Pismeno: {i}: {vysledek[i]}, {round(vysledek[i] / total * 100, 2)}%")
+	#print(f"Pismeno: {i}: {vysledek[i]}, {round(vysledek[i] / total * 100, 2)}%")
+	dict[i] = {'cislo': vysledek[i], 'procento':round(vysledek[i] / total * 100, 2)}
+
+left = range(len(dict))
+
+for i in dict:
+	height = [i]
+#tick_label = dict[0]
+
+#sleep (100)
